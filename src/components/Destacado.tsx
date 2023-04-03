425c910874df3891ea1406269f8a4a6251cb2891
jcsamgit <jcv.gerald@gmail.com>
2023-04-02 21:57:20 -0300

    publicando

---
+++ src/components/Destacado.tsx
@@ -0,0 +1,54 @@
+
+import React, { useEffect, useState, useRef } from 'react';
+// import Aptitudes from './Aptitudes';
+// import Educacion from './Educacion';
+// import Experiencia from './Experiencia';
+// import Logo from './Logo';
+// import Proyectos from './Proyectos';
+
+// export default function Destacado(): JSX.Element {
+//   const [mostrar, setMostrar] = useState<string>('Educación');
+//   const [ultimoBoton, setUltimoBoton] = useState<string>('Educación');
+//   const array: string[] = ['Educación', 'Aptitudes', 'Proyectos', 'Experiencia'];
+
+//   const enfoque = useRef<HTMLButtonElement>(null);
+
+//   useEffect(() => {
+//     // Establece el foco en el div cuando se carga la página
+//     enfoque.current?.focus();
+//   }, []);
+
+//   const handleClick = (item: string): void => {
+//     setMostrar(item);
+//     setUltimoBoton(item);
+//   };
+
+//   return (
+//     <div className='destacado'>
+//       <div className='itemLogoContainer'>
+//         {array.map((item, index) => (
+//           <button
+//             key={item.toString()}
+//             id={item}
+//             className={`itemLogo${ultimoBoton === item ? ' activo' : ''}`}
+//             onClick={() => handleClick(item)}
+//             ref={index === 0 ? enfoque : null} // referencia solo el primer botón
+//           >
+//             <p className='parrafoLogo'>{item}</p>
+//             {/* <div className='logoContainer'> */}
+//             <div className={`logoContainer ${mostrar === item ? 'bbb-activado' : ''}`}>
+//               <Logo />
+//             </div>
+//           </button>
+//         ))}
+//       </div>
+
+//       <div className='contenedorConceptos'>
+//         {mostrar === 'Educación' ? <Educacion /> : false}
+//         {mostrar === 'Aptitudes' ? <Aptitudes /> : false}
+//         {mostrar === 'Proyectos' ? <Proyectos /> : false}
+//         {mostrar === 'Experiencia' ? <Experiencia /> : false}
+//       </div>
+//     </div>
+//   );
+// }
